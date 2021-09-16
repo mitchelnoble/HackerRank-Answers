d307ebdf86c7d29309a7148c6a614ce7043de0ca
mitchel noble <mitchelparkernoble@gmail.com>
2021-09-15 18:15:18 -0700

    changed parameter name

--- loops.js
+++ loops.js
@@ -1,12 +1,17 @@
-function vowelsAndConsonants(word) {
+function vowelsAndConsonants(s) {
   let vowels = [];
   let consonants = [];
-  let newArr = [...word];
+  let newArr = [...s];
 
   for (i = 0; i < newArr.length; i++) {
-    if (newArr[i] == "a" || newArr[i] == "e" || newArr[i] == "i" || newArr[i] == "o" || newArr[i] == "u") {
+    if (
+      newArr[i] == "a" ||
+      newArr[i] == "e" ||
+      newArr[i] == "i" ||
+      newArr[i] == "o" ||
+      newArr[i] == "u"
+    ) {
       vowels.push(newArr[i]);
-
     } else {
       consonants.push(newArr[i]);
     }
@@ -16,4 +21,3 @@ function vowelsAndConsonants(word) {
 }
 
 console.log(vowelsAndConsonants("javascriptloops"));
-
